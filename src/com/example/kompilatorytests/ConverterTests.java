24c509acaaaa66a9bf88bacd308596176f04a39d
AdamSurowka <adamsky21@interia.pl>
2020-06-14 15:34:04 +0200

    Test package separated

---
+++ src/com/example/kompilatorytests/ConverterTests.java
@@ -0,0 +1,14 @@
+package com.example.kompilatorytests;
+
+import com.example.kompilatory.Providers.ConversionType;
+import org.junit.jupiter.api.Test;
+
+
+class ConverterTests {
+
+    @Test
+    public void shouldConvertSampleCsvFileToCorrespondingJson() {
+        ConversionType outputConversionType = ConversionType.JSON;
+
+    }
+}
