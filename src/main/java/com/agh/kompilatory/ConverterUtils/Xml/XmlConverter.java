966c1bcada57ebd0bf38cc3087b30684495e909f
ssysek <33930137+ssysek@users.noreply.github.com>
2020-06-18 00:06:29 +0200

    Update XmlConverter.java

--- src/main/java/com/agh/kompilatory/ConverterUtils/Xml/XmlConverter.java
+++ src/main/java/com/agh/kompilatory/ConverterUtils/Xml/XmlConverter.java
@@ -8,8 +8,6 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.io.FileNotFoundException;
-import java.util.List;
-import java.io.FileNotFoundException;
 
 
 public class XmlConverter implements IConverter {
@@ -46,20 +44,52 @@ public class XmlConverter implements IConverter {
         StringBuilder output = new StringBuilder();
         File file = new File(filePath);
         BufferedReader reader;
+        output = new StringBuilder();
         try {
             reader = new BufferedReader(new FileReader(filePath));
             String line = reader.readLine();
+            String pat1 = ".*<.*>.*</.*>.*";
+            Pattern pattern1 = Pattern.compile(pat1);
+            boolean matchFound;
             while (line != null) {
-                if(line.contains("/")){
-                    System.out.println("kappa");
-                }
-                else if(line.contains("<")){
-                    marks.add(line.replaceAll("\\P{L}+", ""));
-
-                }else {
-                    content.add(line);
-                    output.append(line);
+                Matcher matcher = pattern1.matcher(line);
+                matchFound=matcher.matches();
+                if(line.contains("<?")){
                     output.append("\n");
+                }
+                else if(line.contains("root")){
+                  output.append("[");
+                  output.append("\n");
+                }
+                else if(matchFound){
+                    String buff="";
+                    String kk="";
+                    String kk2="";
+                    String res="";
+                    boolean flag=false;
+                    for (int i = 2; i < line.length(); i++) {
+                        if(line.charAt(i-1)=='>'){
+                            flag=true;
+                        }
+                        if(line.charAt(i)=='<' && line.charAt(i+1)=='/'){
+                            flag=false;
+                        }
+                        if(flag){
+                            buff+=line.charAt(i);
+                        }else{
+                            kk+=line.charAt(i);
+                        }
+                    }
+                    kk=kk.replaceAll("\\P{L}+", "");
+                    kk2 = kk.substring(0, (kk.length()/2));
+                    res="\""+kk2+"\": "+buff;
+                    //System.out.println(res);
+                    output.append(res);
+                    output.append("\n");
+                }else {
+                    output.append("{");
+                    output.append("\n");
+                    //System.out.println("{");
                 }
                 line = reader.readLine();
             }
